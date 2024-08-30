bb0b361ceae159a15eb893e360d71fcedd49e6de
felipestambonidev <felipestambonii@gmail.com>
2024-08-30 01:31:45 -0300

    add: criei o componente do topo ja responsivo

---
+++ src/components/topbar/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { PiShieldCheckBold, PiTruckBold, PiCreditCardBold } from "react-icons/pi";
+import './TopBar.css';
+
+const TopBar: React.FC = () => {
+  return (
+    <header className="top-bar">
+      <div className="top-bar-item">
+        <PiShieldCheckBold className="icon" />
+        <span>Compra <strong>100% segura</strong></span>
+      </div>
+      <div className="top-bar-item">
+        <PiTruckBold className="icon" />
+        <span><strong>Frete grátis</strong> acima de R$ 200</span>
+      </div>
+      <div className="top-bar-item">
+        <PiCreditCardBold className="icon" />
+        <span><strong>Parcele</strong> suas compras</span>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
