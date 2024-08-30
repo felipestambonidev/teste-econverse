371e66f57f2a6369f7861ad391b34fe11c2983e0
felipestambonidev <felipestambonii@gmail.com>
2024-08-30 11:16:13 -0300

    edit: editei o caminho da pag

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/home";
 
 export default function AppRoutes() {
   return (
