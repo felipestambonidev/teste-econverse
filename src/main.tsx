8c8bd2d648b660caff1887f51cf15810ba83d724
felipestambonidev <felipestambonii@gmail.com>
2024-08-30 01:31:02 -0300

    edit: editei o main.tsx fazendo que ele le a pagina de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./globals.css";
+import AppRoutes from './routes/AppRoutes.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>,
 )
