db617b74c1f9b552af00a59c4260120b78c0e9f9
felipestambonidev <felipestambonii@gmail.com>
2024-08-30 01:32:05 -0300

    add: criei o app para rotas

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
