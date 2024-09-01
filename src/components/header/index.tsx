ad604866f9ed673127d9922c38b06eb33f7ba3d4
felipestambonidev <felipestambonii@gmail.com>
2024-09-01 13:15:00 -0300

    add: criei o header, fiz os estilos

---
+++ src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Header.css";
+import { FaSearch } from "react-icons/fa";
+import logovtex from "../../assets/imagesheader/logo.png";
+import iconbox from "../../assets/imagesheader/boxicon.png";
+import iconheart from "../../assets/imagesheader/hearticon.png";
+import iconuser from "../../assets/imagesheader/usericon.png";
+import iconcart from "../../assets/imagesheader/carticon.png";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <section className="header">
+        <div className="logo">
+          <img src={logovtex} alt="VTEX Logo" />
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="O que você está buscando?" />
+          <FaSearch className="search-icon" />
+        </div>
+        <div className="icons">
+          <img src={iconbox} alt="" />
+          <img src={iconheart} alt="" />
+          <img src={iconuser} alt="" />
+          <img src={iconcart} alt="" />
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
