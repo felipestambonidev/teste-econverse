06af0a6a97248018fbd28bc16c8802f8689bd46f
felipestambonidev <felipestambonii@gmail.com>
2024-09-01 11:31:39 -0300

    add: container com banner de fundo finalizado e refatorado

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Banner.css';
+import bannerImage from '../../assets/banner.png'; 
+import { banneritems } from "../../contents/bannerItems"
+
+const Banner: React.FC = () => {
+  return (
+    <div className="banner-container">
+      <img src={bannerImage} alt="Promoção" className="banner-background" />
+      {banneritems.map((item, index) => (
+        <div className="banner-content" key={index}>
+          <h1>{item.title}</h1>
+          <p>{item.paragraph}</p>
+          <button className='banner-button'>{item.buttom}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/contents/bannerItems.ts
@@ -0,0 +1,13 @@
+type TbannerItems = {
+    title: string;
+    paragraph: string;
+    buttom: string;
+};
+
+export const banneritems: TbannerItems[] = [
+    {
+        title: "Venha conhecer nossas promoções",
+        paragraph: "50% Off nos produtos",
+        buttom: "Ver produto"
+    }
+]
