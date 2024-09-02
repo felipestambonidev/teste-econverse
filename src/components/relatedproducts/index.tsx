9544861dee5ef2f3655ad2d6e741f1c6cba36d35
felipestambonidev <felipestambonii@gmail.com>
2024-09-02 17:21:28 -0300

    edit: ajustei o tamanho do titulo

--- src/components/relatedproducts/index.tsx
+++ src/components/relatedproducts/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import './RelatedProducts.scss';
-import Title from '../titlerelatedproducts';
+import React from "react";
+import "./RelatedProducts.scss";
+import Title from "../titlerelatedproducts";
 
 const RelatedProducts: React.FC = () => {
   return (
-    <div className="related-products">
-      <Title title='Produtos Relacionados' />
-      <div className="categories">
-        <div className="category-itempro ative">CELULAR</div>
-        <div className="category-itempro">ACESSÓRIOS</div>
-        <div className="category-itempro">TABLETS</div>
-        <div className="category-itempro">NOTEBOOKS</div>
-        <div className="category-itempro">TVS</div>
-        <div className="category-itempro">VER TODOS</div>
+    <div>
+      <Title title="Produtos Relacionados" />
+      <div className="related-products">
+        <div className="categories">
+          <div className="category-itempro ative">CELULAR</div>
+          <div className="category-itempro">ACESSÓRIOS</div>
+          <div className="category-itempro">TABLETS</div>
+          <div className="category-itempro">NOTEBOOKS</div>
+          <div className="category-itempro">TVS</div>
+          <div className="category-itempro">VER TODOS</div>
+        </div>
       </div>
     </div>
   );
