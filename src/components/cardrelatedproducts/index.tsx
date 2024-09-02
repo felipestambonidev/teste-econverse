3c389bc6484a4d3b416394f6c870fbb50f18baf7
felipestambonidev <felipestambonii@gmail.com>
2024-09-02 18:13:33 -0300

    add: criei e add o componente de card de produtos

--- src/components/cardrelatedproducts/index.tsx
+++ src/components/cardrelatedproducts/index.tsx
@@ -1,13 +1,35 @@
 import Title from "../titlerelatedproducts";
-import "./CardRelatedProduct.scss"
+import "./CardRelatedProduct.scss";
 
 export default function CardRelatedProducts() {
   return (
     <section>
       <Title title="Produtos Relacionados" />
-      <div className="container-relatedproducts">
+      <div>
         <div className="title-h2">
           <h3>Ver todos</h3>
+        </div>
+      </div>
+      <div className="container-relatedproducts">
+        <div className="relprod-card">
+          <div className="shadow-relprod" />
+          <div className="relprod-content">
+            <h2 className="relprod-h2">Produtos</h2>
+            <p className="relprod-p">
+              Lorem ipsum dolor sit amet, consectetur.
+            </p>
+            <button className="buttoncard-relprod">CONFIRA</button>
+          </div>
+        </div>
+        <div className="relprod-card">
+          <div className="shadow-relprod" />
+          <div className="relprod-content">
+            <h2 className="relprod-h2">Produtos</h2>
+            <p className="relprod-p">
+              Lorem ipsum dolor sit amet, consectetur.
+            </p>
+            <button className="buttoncard-relprod">CONFIRA</button>
+          </div>
         </div>
       </div>
     </section>
