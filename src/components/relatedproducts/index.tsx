4320e2f523f5ea114eefe770374148fae442e150
felipestambonidev <felipestambonii@gmail.com>
2024-09-02 01:11:27 -0300

    add: criei e add o componente de produtos relacionados

---
+++ src/components/relatedproducts/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './RelatedProducts.css';
+
+const RelatedProducts: React.FC = () => {
+  return (
+    <div className="related-products">
+      <h2 className="title">Produtos relacionados</h2>
+      <div className="categories">
+        <div className="category-itempro ative">CELULAR</div>
+        <div className="category-itempro">ACESSÓRIOS</div>
+        <div className="category-itempro">TABLETS</div>
+        <div className="category-itempro">NOTEBOOKS</div>
+        <div className="category-itempro">TVS</div>
+        <div className="category-itempro">VER TODOS</div>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,26 @@
-
-import '../globals.css'
-import TopBar from '../components/topbar'
-import Banner from '../components/banner'
-import Header from '../components/header'
-import NavCategories from '../components/navcategories'
-import Categories from '../components/cardcategories'
+import "../globals.css";
+import TopBar from "../components/topbar";
+import Banner from "../components/banner";
+import Header from "../components/header";
+import NavCategories from "../components/navcategories";
+import Categories from "../components/cardcategories";
+import RelatedProducts from "../components/relatedproducts";
 
 function Home() {
-
   return (
-   <div>
-    <header>
-      <TopBar />
-      <Header />
-      <NavCategories />
-    </header>
-    <nav>
-      <Banner />
-      <Categories />
-    </nav>
-   </div>
-  )
+    <div>
+      <header>
+        <TopBar />
+        <Header />
+        <NavCategories />
+      </header>
+      <nav>
+        <Banner />
+        <Categories />
+        <RelatedProducts />
+      </nav>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
