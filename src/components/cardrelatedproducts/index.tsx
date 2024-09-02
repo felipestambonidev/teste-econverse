7b55e8b8b2162edd2eaaedd45b201a1e43963bc5
felipestambonidev <felipestambonii@gmail.com>
2024-09-02 17:35:18 -0300

    add: coloquei um h3 de baixo do titulo

--- src/components/cardrelatedproducts/index.tsx
+++ src/components/cardrelatedproducts/index.tsx
@@ -1,9 +1,15 @@
 import Title from "../titlerelatedproducts";
+import "./CardRelatedProduct.scss"
 
-export default function CardRelatedProducts(){
-    return(
-        <section>
-            <Title title="Produtos Relacionados" />
-        </section>
-    )
+export default function CardRelatedProducts() {
+  return (
+    <section>
+      <Title title="Produtos Relacionados" />
+      <div className="container-relatedproducts">
+        <div className="title-h2">
+          <h3>Ver todos</h3>
+        </div>
+      </div>
+    </section>
+  );
 }
