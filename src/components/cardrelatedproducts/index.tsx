e6fe5f99a3a014f1c8426639c7f2d73b589779c0
felipestambonidev <felipestambonii@gmail.com>
2024-09-03 00:57:36 -0300

    add/edit editei os cards e add o componente de cardproducts

--- src/components/cardrelatedproducts/index.tsx
+++ src/components/cardrelatedproducts/index.tsx
@@ -1,3 +1,4 @@
+import CardProducts from "../cardproducts";
 import Title from "../titlerelatedproducts";
 import "./CardRelatedProduct.scss";
 
@@ -5,29 +6,25 @@ export default function CardRelatedProducts() {
   return (
     <section>
       <Title title="Produtos Relacionados" />
-      <div>
-        <div className="title-h2">
-          <h3>Ver todos</h3>
-        </div>
+      <div className="title-h2">
+        <h3>Ver todos</h3>
       </div>
+      <CardProducts />
+      <div></div>
       <div className="container-relatedproducts">
         <div className="relprod-card">
-          <div className="shadow-relprod" />
+          <div className="shadow-relprod"></div>
           <div className="relprod-content">
             <h2 className="relprod-h2">Produtos</h2>
-            <p className="relprod-p">
-              Lorem ipsum dolor sit amet, consectetur.
-            </p>
+            <p className="relprod-p">Lorem ipsum dolor sit amet, consectetur.</p>
             <button className="buttoncard-relprod">CONFIRA</button>
           </div>
         </div>
         <div className="relprod-card">
-          <div className="shadow-relprod" />
+          <div className="shadow-relprod"></div>
           <div className="relprod-content">
             <h2 className="relprod-h2">Produtos</h2>
-            <p className="relprod-p">
-              Lorem ipsum dolor sit amet, consectetur.
-            </p>
+            <p className="relprod-p">Lorem ipsum dolor sit amet, consectetur.</p>
             <button className="buttoncard-relprod">CONFIRA</button>
           </div>
         </div>
